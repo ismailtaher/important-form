f16266becf426ce81a945cc77cee280a407c7405
Ismail Taher <ismail.taher@ymail.com>
2025-04-11 19:39:25 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Wait for the loading animation to complete (3 seconds)
+  setTimeout(() => {
+    // Redirect to the Rick Astley video
+    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+  }, 3000);
+});
